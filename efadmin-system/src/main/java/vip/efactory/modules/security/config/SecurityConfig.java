5d4c0098e0cd96de9827dd9e6825a0904d81ee24
dbdu <1797890817@qq.com>
2021-09-25 17:57:32 +0800

    移除旧的WebMvc的安装配置及跨域的代码

--- efadmin-system/src/main/java/vip/efactory/modules/security/config/SecurityConfig.java
+++ efadmin-system/src/main/java/vip/efactory/modules/security/config/SecurityConfig.java
@@ -116,58 +116,6 @@ public class SecurityConfig {
                 .build();
     }
 
-//    protected void configure(HttpSecurity httpSecurity) throws Exception {
-//
-//        httpSecurity
-//                // 禁用 CSRF
-//                .csrf().disable()
-//                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-//                // 授权异常
-//                .exceptionHandling()
-//                .authenticationEntryPoint(authenticationErrorHandler)
-//                .accessDeniedHandler(jwtAccessDeniedHandler)
-//
-//                // 防止iframe 造成跨域
-//                .and()
-//                .headers()
-//                .frameOptions()
-//                .disable()
-//
-//                // 不创建会话
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//
-//                .and()
-//                .authorizeRequests()
-//                // 静态资源等等
-//                .antMatchers(
-//                        HttpMethod.GET,
-//                        "/*.html",
-//                        "/**/*.html",
-//                        "/**/*.css",
-//                        "/**/*.js",
-//                        "/webSocket/**"
-//                ).permitAll()
-//                // swagger 文档
-//                .antMatchers("/swagger-ui.html").permitAll()
-//                .antMatchers("/swagger-resources/**").permitAll()
-//                .antMatchers("/webjars/**").permitAll()
-//                .antMatchers("/*/api-docs").permitAll()
-//                // 文件
-//                .antMatchers("/avatar/**").permitAll()
-//                .antMatchers("/file/**").permitAll()
-//                // 阿里巴巴 druid
-//                .antMatchers("/druid/**").permitAll()
-//                // 放行OPTIONS请求
-//                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//                // 自定义匿名访问所有url放行 ： 允许匿名和带权限以及登录用户访问
-//                .antMatchers(anonymousUrls.toArray(new String[0])).permitAll()
-//                // 所有请求都需要认证
-//                .anyRequest().authenticated()
-//                .and().apply(securityConfigurerAdapter());
-//    }
-
     private TokenConfigurer securityConfigurerAdapter() {
         return new TokenConfigurer(tokenProvider);
     }
--- efadmin-system/src/main/java/vip/efactory/modules/security/security/JwtAuthenticationEntryPoint.java
+++ efadmin-system/src/main/java/vip/efactory/modules/security/security/JwtAuthenticationEntryPoint.java
@@ -14,15 +14,6 @@ import reactor.core.publisher.Mono;
 @Component
 public class JwtAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
 
-    // 这是WebMVC的模式
-//    @Override
-//    public void commence(HttpServletRequest request,
-//                         HttpServletResponse response,
-//                         AuthenticationException authException) throws IOException {
-//        // 当用户尝试访问安全的REST资源而不提供任何凭据时，将调用此方法发送401 响应
-//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException==null?"Unauthorized":authException.getMessage());
-//    }
-
     /**
      * 此处可能会有问题，后续研究如何优化，异常信息没有用上
      * @param serverWebExchange 交换机
